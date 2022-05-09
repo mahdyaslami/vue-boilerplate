9209f0b4b45566efa1ad38554f81b7a2b2a42791
mahdi.aslami <mahdi.aslami@partsoftware.com>
2022-05-09 16:32:31 +0430

    Fix importing path in vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [{
-      find: '@', replacement: path.resolve(__dirname, 'src'),
-    }],
-    extensions: ['.vue', '.js'],
-  },
-  server: {
-    host: true,
-  },
+    plugins: [vue()],
+    resolve: {
+        alias: [{
+            find: '@', replacement: path.resolve(__dirname, 'src'),
+        }],
+        extensions: ['.vue', '.js'],
+    },
+    server: {
+        host: true,
+    },
 })
